835f2fdcabc096c2d437907e6f3e48164ecfbe2b
deathcap <deathcap@gmx.co.uk>
2016-04-23 16:46:00 -0700

    Implement most useful asynchronous fs methods
    Found by searching npm installed node_modules:
    ack fs\.|perl -pe's/\(.*//g'|awk -F: '{print $3}'|perl -pe's/.*fs\.//g' |grep -v require|grep -v =|grep -v ' '|sort|uniq

--- host/nodeachrome.js
+++ host/nodeachrome.js
@@ -54,16 +54,95 @@ function messageHandler(msg, push, done) {
       const mode = params[1];
       fs.access(path, mode, cb);
     }
-  } else if (method === 'fs.readFile') {
+  } else if (method === 'fs.chmod') {
     const path = params[0];
+    const mode = params[1];
+    fs.chmod(path, mode, cb);
+  } else if (method === 'fs.chown') {
+    const path = parmas[0];
+    const uid = params[1];
+    const gid = params[2];
+    fs.chown(path, uid, gid, cb);
+  } else if (method === 'fs.close') {
+    const fd = params[0];
+    fs.close(fd, cb);
+    /* TODO
+  } else if (method === 'fs.createReadStream') {
+    const path = params[0];
+    */
+  } else if (method === 'fs.exists') { // deprecated, by npm uses
+    const path = params[0];
+    fs.exists(path, cb);
+  } else if (method === 'fs.fstat') {
+    const fd = params[0];
+    fs.fstat(fd, cb);
+  } else if (method === 'fs.lstat') {
+    const path = params[0];
+    fs.lstat(path, cb);
+  } else if (method === 'fs.open') {
+    const path = params[0];
+    const flags = params[1];
+    if (params.length < 3) {
+      const mode = params[2];
+      fs.open(path, flags, mode, cb);
+    } else {
+      fs.open(path, flags, cb);
+    }
+  } else if (method === 'fs.readFile') {
+    const file = params[0];
     // TODO: restrict access to only a limited set of files
     if (params.length < 2) {
       //const callback = params[1];
-      fs.readFile(path, cb);
+      fs.readFile(file, cb);
     } else {
       const options = params[1];
       //const callback = params[2];
-      fs.readFile(path, options, cb);
+      fs.readFile(file, options, cb);
+    }
+  } else if (method === 'fs.readdir') {
+    const path = params[0];
+    fs.readdir(path, cb);
+  } else if (method === 'fs.readlink') {
+    const path = params[0];
+    fs.readlink(path, cb);
+  } else if (method === 'fs.realpath') {
+    const path = params[0];
+    if (params.length < 2) {
+      fs.realpath(path, cb);
+    } else {
+      const cache = params[1];
+      fs.realpath(path, cache, cb);
+    }
+  } else if (method === 'fs.rename') {
+    const oldPath = params[0];
+    const newPath = params[1];
+    fs.rename(oldPath, newPath, cb);
+  } else if (method === 'fs.rmdir') {
+    const path = params[0];
+    fs.rmdir(path, cb);
+  } else if (method === 'fs.stat') {
+    const path = params[0];
+    fs.stat(path, cb);
+  } else if (method === 'fs.symlink') {
+    const target = params[0];
+    const path = params[1];
+    if (params.length < 3) {
+      fs.symlink(target, path, cb);
+    } else {
+      const type = params[2];
+      fs.symlink(target, path, type, cb);
+    }
+  } else if (method === 'fs.unlink') {
+    const path = params[0];
+    fs.unlink(path, cb);
+  } else if (method === 'fs.writeFile') {
+    const file = params[0];
+    const data = params[1];
+    if (params.length < 3) {
+      fs.writeFile(file, data, cb);
+    } else {
+      const options = params[2];
+      fs.writeFile(file, data, options, cb);
     }
   } else {
     push({error: {
