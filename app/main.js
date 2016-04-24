3e98eeff7c07fa0a9f4839c5e7116efa07364c86
deathcap <deathcap@gmx.co.uk>
2016-04-24 13:14:56 -0700

    Move process augmentation to more-process.js

--- app/main.js
+++ app/main.js
@@ -2,39 +2,7 @@
 
 require('shellasync/global'); // export some useful shell-like functions: cat(), ls(), ... using node.js fs async APIs
 require('./ui')(); // wire up button event handlers
-
-// Extend the process global with some more functionality
-// https://nodejs.org/api/process.html
-Object.assign(process.env, {
-  TERM: 'xterm-256color',
-  SHELL: '/bin/sh',
-  USER: 'user',
-  LOGNAME: 'user',
-  PATH: '~/.bin/:/usr/bin/:/bin:/usr/sbin:/sbin:/usr/local/bin',
-  PWD: '/',
-  HOME: '/',
-});
-process.exit = (code) => {
-  console.log(`process.exit(${code})`);
-};
-Object.assign(process.versions, {
-  node: '4.2.4', // simulated node.js compatibility level version (optimistic)
-  app: navigator.appVersion,
-  webkit: navigator.appVersion.match(/WebKit\/([\d.]+)/)[1],
-  chrome: navigator.appVersion.match(/Chrome\/([\d.]+)/)[1],
-  // TODO: how can we get v8 version? if at all, from Chrome version? node process.versions has it
-});
-process.version = process.versions.node;
-process.cwd = () => {
-  return process.env.PWD;
-};
-process.execPath = '/bin/node';
-const BrowserStdout = require('browser-stdout');
-process.stdout = BrowserStdout();
-process.stderr = BrowserStdout({label: 'stderr'}); // TODO: console.error instead of console.log?
-// TODO: stdin
-
-
+require('./more-process');
 
 // Expose globally for debugging
 global.g = {
