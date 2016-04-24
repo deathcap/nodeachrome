c4d788a31acc65f3052d129d0972f0f346ad210d
deathcap <deathcap@gmx.co.uk>
2016-04-23 22:59:46 -0700

    Expose fs, process, and browserify globally

--- app/main.js
+++ app/main.js
@@ -3,7 +3,8 @@
 require('shellasync/global'); // export some useful shell-like functions: cat(), ls(), ... using node.js fs async APIs
 require('./ui')(); // wire up button event handlers
 
-const fs = require('fs');
-const process = require('process');
-//const npm = require('npm'); // crashes at:   ReadStream.prototype = Object.create(fs$ReadStream.prototype), no fs.ReadStream defined
-const browserify = require('browserify');
+// Expose globally for debugging
+global.fs = require('fs');
+global.process = require('process');
+//global.npm = require('npm'); // crashes at:   ReadStream.prototype = Object.create(fs$ReadStream.prototype), no fs.ReadStream defined
+global.browserify = require('browserify');
