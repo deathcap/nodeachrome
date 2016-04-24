c3be97c0c9153823d2cd6cba1106b41af1abed97
deathcap <deathcap@gmx.co.uk>
2016-04-23 22:46:29 -0700

    Add static file data for ./node_modules/browser-pack/_prelude.js

--- app/fs.js
+++ app/fs.js
@@ -45,8 +45,10 @@ fs.readFile = (path, options, cb) => {
 };
 
 const STATIC_FILE_DATA = {
-  '/greeting': 'hello world',
-  // TODO: Buffers (browserified nodejs)
+  '/greeting': new Buffer('hello world'),
+
+  // cat ./node_modules/browser-pack/_prelude.js
+  '/node_modules/browser-pack/_prelude.js': new Buffer(`(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})`),
 };
 
 fs.readFileSync = (file, options) => {
