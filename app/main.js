c46e9a095e8a5dcb7db7b2393e59ee2347ac515b
deathcap <deathcap@gmx.co.uk>
2016-04-23 17:42:00 -0700

    Add some mostly inconsequential process module implementation

--- app/main.js
+++ app/main.js
@@ -62,7 +62,8 @@ function sendNative(method, params, cb) {
   chrome.runtime.sendNativeMessage(application, msg, (response) => decodeResponse(response, cb));
 }
 
-let fs = {};
+// https://nodejs.org/api/fs.html
+const fs = {};
 
 fs.access = (path, mode, cb) => {
   if (!cb) {
@@ -150,4 +151,30 @@ fs.writeFile = (file, data, options, cb) => {
   } else {
     sendNative('fs.writeFile', [file, data, options], cb);
   }
+};
+
+
+// https://nodejs.org/api/process.html
+const process = {};
+
+process.arch = 'wasm';
+process.env = {
+  TERM: 'xterm-256color',
+  SHELL: '/bin/sh',
+  USER: 'user',
+  LOGNAME: 'user',
+  PATH: '~/.bin/:/usr/bin/:/bin:/usr/sbin:/sbin:/usr/local/bin',
+  PWD: '/Users/user',
+  HOME: '/Users/user',
+};
+process.exit = (code) => {
+  console.log(`process.exit(${code})`);
+}
+process.platform = 'chrome';
+process.versions = {
+  node: '4.2.4', // simulated node.js compatibility level version (optimistic)
+  app: navigator.appVersion,
+  webkit: navigator.appVersion.match(/WebKit\/([\d.]+)/)[1],
+  chrome: navigator.appVersion.match(/Chrome\/([\d.]+)/)[1],
+  // TODO: how can we get v8 version? if at all, from Chrome version? node process.versions has it
 };
