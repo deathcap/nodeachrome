13dfed309c0532399ef3b86bce3a6d74816e5102
deathcap <deathcap@gmx.co.uk>
2016-04-23 22:08:26 -0700

    Move setting <button> event handlers to ui.js

--- app/main.js
+++ app/main.js
@@ -1,57 +1,9 @@
 'use strict';
 
-const application = 'io.github.deathcap.nodeachrome';
-
 require('shellasync/global'); // export some useful shell-like functions: cat(), ls(), ... using node.js fs async APIs
+require('./ui')(); // wire up button event handlers
 
-let port = null;
-
-document.getElementById('connect').addEventListener('click', () => {
-  log('chrome.runtime.connectNative');
-
-  port = chrome.runtime.connectNative(application);
-  port.onMessage.addListener(log);
-  port.onDisconnect.addListener((e) => {
-    log('unexpected disconnect');
-    port = null;
-  });
-});
-
-document.getElementById('disconnect').addEventListener('click', () => {
-  log('port.disconnect');
-  port.disconnect();
-  port = null;
-});
-
-document.getElementById('send').addEventListener('click', () => {
-  const json = document.getElementById('msg').value;
-  let msg;
-
-  try {
-    msg = JSON.parse(json);
-  } catch (err) {
-    return log('invalid JSON: ' + json);
-  }
-
-  if (port) {
-    log('port.postMessage');
-    port.postMessage(msg);
-  } else {
-    log('chrome.runtime.sendNativeMessage');
-    chrome.runtime.sendNativeMessage(application, msg, log);
-  }
-});
-
-document.getElementById('clear').addEventListener('click', () => {
-  document.getElementById('log').innerHTML = '';
-});
-
-function log(msg) {
-  console.log(msg);
-
-  const e = document.createElement('pre');
-  e.appendChild(document.createTextNode(typeof msg === 'object' ? JSON.stringify(msg) : msg));
-  document.getElementById('log').appendChild(e);
-}
-
-
+const fs = require('fs');
+const process = require('process');
+//const npm = require('npm'); // crashes at:   ReadStream.prototype = Object.create(fs$ReadStream.prototype), no fs.ReadStream defined
+const browserify = require('browserify');
