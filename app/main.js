9d4f86ae64edd8621ff1c00c2d2a837309a2ebc7
deathcap <deathcap@gmx.co.uk>
2016-04-23 17:01:19 -0700

    Refactor Chrome extension decodeResponse/sendNative

--- app/main.js
+++ app/main.js
@@ -51,13 +51,19 @@ function log(msg) {
   document.getElementById('log').appendChild(e);
 }
 
+function decodeResponse(response, cb) {
+  if (!response) return cb(chrome.runtime.lastError);
+  if (response.error) return cb(new Error(response.error.message));
+  return cb(null, response.result);
+}
+
+function sendNative(method, params, cb) {
+  const msg = {method: method, params: params};
+  chrome.runtime.sendNativeMessage(application, msg, (response) => decodeResponse(response, cb));
+}
+
 let fs = {};
 fs.readFile = (path, options, cb) => {
   if (!cb) cb = options;
-  const msg = {method: 'fs.readFile', params: [path, options]}; // TODO: 1-argument variation, would like to use arguments, or rest parameters but...
-  chrome.runtime.sendNativeMessage(application, msg, (response) => {
-    if (!response) return cb(chrome.runtime.lastError);
-    if (response.error) return cb(new Error(response.error.message));
-    return cb(null, response.result);
-  });
+  sendNative('fs.readFile', [path, options], cb); // TODO: 1-argument variation, would like to use arguments, or rest parameters but...
 };
