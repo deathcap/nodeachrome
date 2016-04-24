70503a55c05c0f81948ee48dda43441ae4bb7341
deathcap <deathcap@gmx.co.uk>
2016-04-24 01:01:31 -0700

    Add much of process/browser module for npm

--- app/process.js
+++ app/process.js
@@ -7,6 +7,98 @@
 // https://github.com/defunctzombie/node-process/blob/master/browser.js
 const process = {};
 
+// based on https://github.com/defunctzombie/node-process/blob/master/browser.js TODO: refactor
+// removed some functions, implemented elsehow
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = setTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    clearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        setTimeout(drainQueue, 0);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
+/////////////////////////////////////////////////////////////////////////////
+// custom process enhancements
+
 const Writable = require('stream').Writable;
 
 process.arch = 'wasm';
@@ -31,7 +123,10 @@ process.versions = {
   // TODO: how can we get v8 version? if at all, from Chrome version? node process.versions has it
 };
 process.version = process.versions.node;
-process.title = 'browser';
+process.argv = ['npm'];
+process.cwd = () => {
+  return process.env.PWD;
+};
 
 process.stdout = Writable();
 process.stdout._write = (chunk, enc, next) => {
@@ -44,5 +139,6 @@ process.stderr._write = (chunk, enc, next) => {
   next();
 };
 // TODO: stdin
+
 
 module.exports = process;
