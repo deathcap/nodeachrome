04d6079da1e27dee4af3e0669353d69e49fcebc5
deathcap <deathcap@gmx.co.uk>
2016-04-24 10:13:56 -0700

    Import most browserified modules to make them available in the console when debugging

--- app/main.js
+++ app/main.js
@@ -6,8 +6,33 @@ require('./ui')(); // wire up button event handlers
 // Expose globally for debugging
 global.g = {
   require: require,
-  fs: require('fs'),
-  process: require('process'),
+
+  // Pull in most implemented from https://github.com/substack/node-browserify/blob/master/lib/builtins.js
+  assert: require('assert'),
+  buffer: require('buffer'),
+  console: require('console'),
+  constants: require('constants'),
+  crypto: require('crypto'),
+  domain: require('domain'),
+  events: require('events'),
+  fs: require('fs'), // our own ./fs.js
+  http: require('http'),
+  https: require('https'),
+  os: require('os'),
+  path: require('path'),
+  querystring: require('querystring'),
+  stream: require('stream'),
+  string_decoder: require('string_decoder'),
+  sys: require('sys'),
+  timers: require('timers'),
+  tty: require('tty'),
+  url: require('url'),
+  util: require('url'),
+  vm: require('vm'),
+  zlib: require('zlib'),
+  process: require('process'), // our own ./process.js, is _process
+
+  // Useful apps
   browserify: require('browserify'),
   npm: require('npm'),
   setup_npm: setup_npm,
