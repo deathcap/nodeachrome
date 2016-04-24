f52c3974421e6b3f9bc2915257e328e7e6b6d08a
deathcap <deathcap@gmx.co.uk>
2016-04-23 23:50:19 -0700

    readFileSync implementation: respect encoding, fixes browserify prelude src.match undefined

--- app/fs.js
+++ app/fs.js
@@ -49,7 +49,7 @@ const STATIC_FILE_DATA = {
 
   // cat ./node_modules/browser-pack/_prelude.js
   // based on https://github.com/substack/browser-pack/blob/01d39894f7168983f66200e727cdaadf881cd39d/prelude.js
-  // also prelude2.js
+  // TODO: also prelude2.js
   '/node_modules/browser-pack/_prelude.js': new Buffer(`(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})`),
 };
 
@@ -64,7 +64,14 @@ fs.readFileSync = (file, options) => {
     throw e;
   }
 
-  return data;
+  let encoding = null;
+  if (typeof options === 'string') {
+    encoding = options;
+  } else if (typeof options === 'object') {
+    encoding = options.encoding;
+  }
+
+  return data.toString(encoding);
 };
 
 fs.readdir = (path, cb) => sendNative('fs.readdir', [path], cb);
