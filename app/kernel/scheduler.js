c8c1e720d538501805305b252eefa5cd356f6e0a
deathcap <deathcap@gmx.co.uk>
2016-04-30 13:20:11 -0700

    Add initial implementation of Process class

--- app/kernel/scheduler.js
+++ app/kernel/scheduler.js
@@ -8,6 +8,53 @@ let iframes = new Map();
 
 const {createDraggableIframe} = require('./windowing');
 
+class Process {
+  constructor() {
+    this.state = 'new'; // see https://en.wikipedia.org/wiki/Process_state
+    this.pid = nextPid;
+    nextPid += 1;
+
+    let {iframe, container} = createDraggableIframe(pid);
+
+    this.iframe = iframe;
+    this.container = container;
+
+    iframes.set(pid, iframe);
+
+    const containers = document.getElementById('userland-processes');
+    containers.appendChild(container);
+
+    console.log(`new Process: pid=${this.pid}`);
+    // process is created in frozen state, can be started with exec()
+  }
+
+  exec(argv, env) {
+    if (!env) env = global.ENV; // inherit from kernel TODO: per-process inheritance, forking
+
+    this.state = 'waiting'; // awaiting execution
+    iframe.setAttribute('src', '/userland/userland.html');
+    iframe.addEventListener('load', (event) => {
+      console.log('sandbox frame load',pid);
+      iframe.contentWindow.postMessage({cmd: '_start', pid: pid, argv, env}, '*');
+      this.state = 'running'; // TODO: or should differentiate between frame loaded, and confirmed the frame is running the script? get feedback from _start
+    });
+  }
+
+  postMessage(msg) {
+    if (!this.iframe.contentWindow) throw new Error(`no such process, terminated: ${this.pid}`);
+
+    this.iframe.contentWindow.postMessage(msg, '*');
+  }
+
+  terminate() {
+    this.iframe.parentNode.removeChild(iframe); // TODO: remove container???
+    this.iframes.delete(pid);
+    console.log(`Terminated ${pid}`);
+    this.state = 'terminated';
+    // TODO: reap zombies
+  }
+}
+
 // Create a new "userland" sandboxed execution context, previously 'newsb',
 // like Unix spawn/exec (close enough) or posix_spawn/system
 // TODO: Unix-ish standard Node API to implement instead? process, os? exec? Found it: https://nodejs.org/api/child_process.html exec!
