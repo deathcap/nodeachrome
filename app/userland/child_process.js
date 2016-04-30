87d146fe15730e8f7300eb58944f61fb83fdd9cc
deathcap <deathcap@gmx.co.uk>
2016-04-30 15:51:24 -0700

    Add child_process.exec(), calls into .spawn()

--- app/userland/child_process.js
+++ app/userland/child_process.js
@@ -4,7 +4,19 @@
 //
 // https://nodejs.org/api/child_process.html
 
+const EventEmitter = require('events').EventEmitter;
 const syscall = require('./syscall').syscall;
+
+class ChildProcess extends EventEmitter {
+  constructor() {
+    // TODO
+    this.pid = null;
+  }
+
+  kill(signal='SIGTERM') {
+    process.kill(this.pid, signal);
+  }
+}
 
 // https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
 function spawn(command, args=[], options={}) {
@@ -13,10 +25,24 @@ function spawn(command, args=[], options={}) {
   const env = options.env !== undefined ? options.env : process.env; // inherit from parent
 
   syscall({cmd: 'spawn', command, args, env});
+  // TODO: how can we get a pid immediately? given that syscalls are async?!
 
-  // TODO: return a new ChildProcess
+  return new ChildProcess();
+}
+
+function exec(command_string, options, cb) {
+  const command_args = command_string.split(' '); // "The command to run, with space-separated arguments"
+  const command = command_args[0];
+  const args = command_args.slice(1);
+
+  return spawn(command, args, options);
+}
+
+function fork(modulePath, args, options) {
+  // TODO: require(modulePath) or what?
 }
 
 module.exports = {
   spawn,
+  exec,
 };
