d15ea293b89c52543814b6cd2b0bd32d1fed796c
deathcap <deathcap@gmx.co.uk>
2016-04-30 16:15:13 -0700

    Track next process ID to return sync from child_process.spawn()

--- app/userland/child_process.js
+++ app/userland/child_process.js
@@ -7,10 +7,13 @@
 const EventEmitter = require('events').EventEmitter;
 const syscall = require('./syscall').syscall;
 
+let nextPid = null;
+
 class ChildProcess extends EventEmitter {
-  constructor() {
-    // TODO
-    this.pid = null;
+  constructor(pid) {
+    super();
+
+    this.pid = pid;
   }
 
   kill(signal='SIGTERM') {
@@ -18,16 +21,26 @@ class ChildProcess extends EventEmitter {
   }
 }
 
+window.addEventListener('message', (event) => {
+  if (event.data.cmd === 'nextPid') {
+    // Received when a kernel spawns a new process, with the next expected process ID
+    nextPid = event.data.nextPid;
+  }
+});
+
 // https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
 function spawn(command, args=[], options={}) {
  
   const cwd = options.cwd !== undefined ? options.cwd : process.cwd(); // hmm, but docs say defaults to 'undefined
   const env = options.env !== undefined ? options.env : process.env; // inherit from parent
 
-  syscall({cmd: 'spawn', command, args, env});
-  // TODO: how can we get a pid immediately? given that syscalls are async?!
+  // we have to return synchronously, so hope the child's pid will be nextPid
+  // (passed and checked in kernel spawn handler to detect races)
+  const childPid = nextPid;
 
-  return new ChildProcess();
+  syscall({cmd: 'spawn', command, args, env, expectedPid: childPid});
+
+  return new ChildProcess(childPid);
 }
 
 function exec(command_string, options, cb) {
