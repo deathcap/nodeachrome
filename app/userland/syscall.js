c0c72a8091dae191cdd29ec22882d0f09c574280
deathcap <deathcap@gmx.co.uk>
2016-04-30 19:20:03 -0700

    Refactor to use dispatch table for command execution

--- app/userland/syscall.js
+++ app/userland/syscall.js
@@ -27,14 +27,16 @@ window.addEventListener('message', (event) => {
 
     event.source.postMessage({cmd: 'started'}, event.origin);
 
-    if (process.argv[1] === 'init') {
-      require('./bin/init');
-    } else if (process.argv[1] === 'npm') {
-      require('./bin/npm');
-    } else if (process.argv[1] === 'browserify') {
-      require('./bin/browserify');
-    } else {
-      // TODO: dynamic requires
+    // TODO: dynamic requires
+    // This is what we can do now with browserify
+    let commands = {
+      init: () => require('./bin/init'),
+      npm: () => require('./bin/npm'),
+      browserify: () => require('./bin/browserify'),
+    };
+
+    if (commands[process.argv[1]]) {
+      commands[process.argv[1]]();
     }
   }
 });
