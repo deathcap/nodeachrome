0e8710d120732b4ad49a74eb4852d9651a9e2a93
deathcap <deathcap@gmx.co.uk>
2016-05-05 21:51:44 -0700

    Fix lossy stdout message decoding redir and receiving only first (hack)

--- host/cli.js
+++ host/cli.js
@@ -41,40 +41,84 @@ rs._read = (size) => {
 };
 
 const Writable = require('stream').Writable;
-const ws = new Writable({objectMode: true});
-ws._write = (chunk, encoding, cb) => {
-  console.log('writable received',chunk,encoding);
-  // TODO: get data back
-};
 
 rs
 .pipe(new nativeMessage.Output())
-.pipe(client)
+.pipe(client);
 
+const ws = new Writable({objectMode: true});
+ws._write = (msg, encoding, cb) => {
+  //console.log('msg',msg);
+
+  if (msg.cmd === 'stdout') {
+    process.stdout.write(msg.output);
+  } else if (msg.cmd === 'ack') {
+    console.log('Host received',msg);
+  } else {
+    console.log('Unknown message:',msg);
+  }
+};
+
+// Decode all messages
+// TODO: find out why I have to do this instead of the straightforward approach in
+// https://github.com/deathcap/nodeachrome/pull/30#issuecomment-217346189
+// which only reads the first message
+const decodeState = {
+  buf: Buffer.alloc(0),
+  push: (msg) => {
+    ws._write(msg, null, () => {});
+  },
+};
 client.on('readable', () => {
   //console.log('client is readable');
-  const data = client.read();
+  const chunk = client.read();
 
-  if (data) {
-    // Decode the buffer to JSON
-    //console.log('read=', data.toString());
-
-    const rs = new Readable;
-    const ws = new Writable({objectMode: true});
-    ws._write = (msg, encoding, cb) => {
-      //console.log('msg',msg);
-
-      if (msg.cmd === 'stdout') {
-        process.stdout.write(msg.output);
-      } else if (msg.cmd === 'ack') {
-        console.log('Host received',msg);
-      } else {
-        console.log('Unknown message:',msg);
-      }
-    };
-    rs.push(data);
-    rs.push(null);
-    rs.pipe(new nativeMessage.Input()).pipe(ws);
+  function done() {
+    //console.log('Done!', decodeState); // done decoding this message
   }
-  //client.pipe(new nativeMessage.Input()).pipe(ws);
+
+  // based on https://github.com/jdiamond/chrome-native-messaging/blob/master/index.js#L38
+  // Input.prototype._transform = function(chunk, encoding, done) {
+ 
+    // Save this chunk.
+    var self = decodeState;
+    self.buf = Buffer.concat([ self.buf, chunk ]);
+
+    function parseBuf() {
+        // Do we have a length yet?
+        if (typeof self.len !== 'number') {
+            // Nope. Do we have enough bytes for the length?
+            if (self.buf.length >= 4) {
+                // Yep. Parse the bytes.
+                self.len = self.buf.readUInt32LE(0);
+                // Remove the length bytes from the buffer.
+                self.buf = self.buf.slice(4);
+            }
+        }
+
+        // Do we have a length yet? (We may have just parsed it.)
+        if (typeof self.len === 'number') {
+            // Yep. Do we have enough bytes for the message?
+            if (self.buf.length >= self.len) {
+                // Yep. Slice off the bytes we need.
+                var message = self.buf.slice(0, self.len);
+                // Remove the bytes for the message from the buffer.
+                self.buf = self.buf.slice(self.len);
+                // Clear the length so we know we need to parse it again.
+                self.len = null;
+                // Parse the message bytes.
+                var obj = JSON.parse(message.toString());
+                // Enqueue it for reading.
+                self.push(obj);
+                // We could have more messages in the buffer so check again.
+                parseBuf();
+            }
+        }
+    }
+
+    // Check for a parsable buffer (both length and message).
+    parseBuf();
+
+    // We're done.
+    done();
 });
